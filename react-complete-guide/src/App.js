17770491e561275ea7068d53c5deff3a7c13846a
VAJ-391999 <venishachauhan1234@gmail.com>
2021-05-11 18:51:33 +0530

    first Commit

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ]
+  }
+
+  nameChangeHandler =(event) => {
+    this.setState({persons: [
+      {name: 'Max', age: 28},
+      {name: event.target.value, age: 29},
+      {name: 'Stephanie', age: 26}
+    ]})
+  }
+
+  switchNameHandler = (newName) => {
+    //console.log('was clicked!');
+    this.setState({persons: [
+      {name: newName, age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 27}
+    ]})
+  }
+
+  render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App</h1>
+        <button 
+          style={style}
+          onClick={() =>this.switchNameHandler('Maximilion!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this,'Max!')}
+          changed={this.nameChangeHandler}>My Hobbies : Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
+}
+
+export default App;
