042bd500f473d995a32be5c9002fb93cadc0b06a
VAJ-391999 <venishachauhan1234@gmail.com>
2021-05-12 18:32:52 +0530

    Practice 2

---
+++ react-complete-guide/src/containers/App.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+/*import Radium, { StyleRoot } from 'radium';*/
+import './App.css';
+import Person from '../components/Persons/Person/Person';
+import ErrorBoundary from '../ErrorBoundary/ErorBoundary';
+
+class App extends Component {
+  state = {
+    persons: [
+      {id: 'as1', name: 'Max', age: 28},
+      {id: 'as2',name: 'Manu', age: 29},
+      {id: 'as3',name: 'Stephanie', age: 26}
+    ],
+    showPerson: false
+  }
+
+  nameChangeHandler =(event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
+  }
+
+  deletePersonHandler = (personIndex) => {
+     //const persons = this.state.persons.slice();
+      const persons = [...this.state.persons]
+     persons.splice(personIndex,1);
+     this.setState({persons: persons})
+  }
+
+  togglePersonHandler = () => {
+    const doseShow = this.state.showPerson;
+    this.setState({showPerson: !doseShow});
+  }
+
+  render() {
+
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      ':hover' : {
+        backgroundColor : 'lightgreen',
+        color: 'black'
+      }
+    };
+
+    let persons = null;
+
+    if (this.state.showPerson) {
+      persons = (
+        <div>
+          {
+            this.state.persons.map((person, index) => {
+            return <ErrorBoundary key={person.id}>
+              <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name} 
+              age={person.age}
+              
+              changed={(event) => this.nameChangeHandler(event, person.id)}/>
+              </ErrorBoundary>
+          })}
+           
+         </div>
+      ); 
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor : 'salmon',
+        color: 'black'
+      };
+    }
+
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
+    return (
+      
+        <div className="App">
+        <h1>Hi, I am a React App</h1>
+        <p className={classes.join(' ')}>This is really working!!</p>
+        <button 
+          style={style}
+          onClick={this.togglePersonHandler}>Toggle Person</button>
+          {persons} 
+      </div>
+      
+      
+    );
+  }
+}
+
+export default App;
